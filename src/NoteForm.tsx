463e0e5f861f9d90c48fdae676bbd02b6c04868a
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-26 02:35:09 -0400

    Implement add and set tags  when submitting note

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -3,12 +3,16 @@ import { Form, Stack, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "./App";
+import { v4 as uuidV4 } from "uuid";
+
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
 
-export function NoteForm({ onSubmit }: NoteFormProps) {
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null)
   const markdownRef = useRef<HTMLTextAreaElement>(null)
   const [selectedTags, setSelectedTags] = useState<Tag[]>([])
@@ -18,7 +22,7 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: []
+      tags: selectedTags
     })
   }
   return (
@@ -34,9 +38,17 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect 
+              <CreatableReactSelect
+              onCreateOption={label => {
+                const newTag = {id: uuidV4(), label}
+                onAddTag(newTag)
+                setSelectedTags(prev => [...prev, newTag])
+              }}
               value={selectedTags.map(tag => {
                 return { label: tag.label, value:tag.id }
+              })}
+              options={availableTags.map(tag => {
+                return {label: tag.label, value: tag.id}
               })}
               onChange={tags => {
                 setSelectedTags(
