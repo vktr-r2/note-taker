b1b628554ceba609c128f843d76b76d79b11d22d
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-26 01:54:03 -0400

    implement useMemo to cache note and assigned tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
 import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
 
 export type Note = {
   id: string
@@ -10,7 +11,7 @@ export type Note = {
 
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string
@@ -31,6 +32,13 @@ export type Tag = {
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
   return (
     <Container className="my-4">
       <Routes>
