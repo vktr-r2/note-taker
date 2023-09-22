6c7b13eee4ab32ac61b6455650adac54d71e8745
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 17:48:53 -0400

    Add comments to useLocalStorage custom hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,23 +1,35 @@
-import {useEffect, useState} from "react";
+// React hooks for managing state and side-effects
+import { useEffect, useState } from "react";
 
+// Declare useLocalStorage with generic type param T
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+
+  // Initialize local state with useState
   const [value, setValue] = useState<T>(() => {
-    const jsonValue = localStorage.getItem(key)
+
+    // Retrieve item from localStorage
+    const jsonValue = localStorage.getItem(key);
+    // Check if item exists in localStorage
     if (jsonValue == null) {
+
+      // If initialValue is function, call function to get the actual value
+      // Otherwise, use initialValue as is
       if (typeof initialValue === "function") {
-        return (initialValue as () => T)()
+        return (initialValue as () => T)();
       } else {
-        return initialValue
+        return initialValue;
       }
-
     } else {
-      return JSON.parse(jsonValue)
+      // If the item exists in localStorage, parse JSON value
+      return JSON.parse(jsonValue);
     }
-  })
+  });
 
+  // useEffect to set the localStorage whenever the value changes
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
-  }, [value, key])
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
 
-  return [value, setValue] as [T, typeof setValue]
+  // Return the current value and the function to update it
+  return [value, setValue] as [T, typeof setValue];
 }
