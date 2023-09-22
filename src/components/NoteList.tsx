bd4e3576ea5bdf14c86aba240f2813ff964c6830
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 19:51:38 -0400

    Add Create and Edit Tags buttons

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,15 +1,24 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Stack } from "react-bootstrap";
+import  Button  from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 // NoteList component
 export function NoteList() {
-  return <>
-    <Row>
-      <Col>
-        <h1> Notes </h1>
-      </Col>
-      <Col>
-        <h3> Buttons Here </h3>
-      </Col>
-    </Row>
-  </>
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1> Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary" size="lg">Create</Button>
+            </Link>
+            <Button variant="outline-secondary" size="lg">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+    </>
+  );
 }
