7ba1103b08d4853a6bb2a054755b8d08f23d72c5
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 17:26:45 -0400

    Add comments to main component

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+// Get a root element from the HTML file with ID "root" and create a React root.
+// Then render the component tree into the root element.
+// (!) asserts that the element will be non-null.
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
