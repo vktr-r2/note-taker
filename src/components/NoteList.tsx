6b8f5057fa2d4870be27dfa2adf65f2a67ebc9e7
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-23 00:14:25 -0400

    Add comments to filteredNotes hook

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -19,17 +19,23 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
   // Init state for title search form
   const [title, setTitle] = useState("");
 
-  // filteredNotes useMemo hook to filter NoteCard components on home
+  // useMemo to reduce unnecissary renders
   const filteredNotes = useMemo(() => {
+    // filter method on 'notes' array to make new array of notes
     return notes.filter((note) => {
-      return (
-        (title === "" ||
-          note.title.toLowerCase().includes(title.toLowerCase())) &&
-        (selectedTags.length === 0 ||
-          selectedTags.every((tag) =>
-            note.tags.some((noteTag) => noteTag.id == tag.id)
-          ))
-      );
+      // Note returned in filtered array if it meets BOTH criteria:
+      // 1. 'title' search string is empty OR note's title (case sensitive) includes the 'title' search string
+      const titleCondition =
+        title === "" || note.title.toLowerCase().includes(title.toLowerCase());
+      // 2. no tags are selected OR every tag in 'selectedTags' must exist in 'tags' array of the note
+      const tagCondition =
+        selectedTags.length === 0 ||
+        selectedTags.every((tag) =>
+          note.tags.some((noteTag) => noteTag.id === tag.id)
+        );
+
+      // Return TRUE if titleCondition and tagCondition are met, including that note in filtered results
+      return titleCondition && tagCondition;
     });
   }, [title, selectedTags, notes]);
 
