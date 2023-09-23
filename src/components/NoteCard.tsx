f9f7a5966599fae41560779ca86b766168c48529
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-23 10:57:23 -0400

    Add title and tags content to NoteCard component

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Badge, Card, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Tag } from "../App";
 import styles from "../styles/NoteList.module.css"
@@ -13,8 +13,16 @@ export function NoteCard({ id, title, tags }: SimplifiedNote) {
   return (
     <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
       <Card.Body>
-        <h1>{}</h1>
-        <p>{title}</p>
+        <Stack gap={2} className="align-items-center justify-content-center h-100">
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+              {tags.map(tag => (
+                <Badge className="text-truncates" key={tag.id}>{tag.label}</Badge>
+              ))}
+              </Stack>
+          )}
+        </Stack>
       </Card.Body>
     </Card>
   );
