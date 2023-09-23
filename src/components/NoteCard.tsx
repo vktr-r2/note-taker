85175e565b3a17740132924b69a5fa917c08f670
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-23 10:37:50 -0400

    Style NoteCard component

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,21 +1,21 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Tag } from "../App";
-
+import styles from "../styles/NoteList.module.css"
 
 export type SimplifiedNote = {
-  tags: Tag[]
-  title: string
-  id: string
-}
+  tags: Tag[];
+  title: string;
+  id: string;
+};
 
-export function NoteCard({id, title, tags}: SimplifiedNote) {
+export function NoteCard({ id, title, tags }: SimplifiedNote) {
   return (
-    <Card as={Link} to={`/${id}`} >
+    <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
       <Card.Body>
+        <h1>{}</h1>
         <p>{title}</p>
       </Card.Body>
-
-    </Card>>
-  )s
+    </Card>
+  );
 }
