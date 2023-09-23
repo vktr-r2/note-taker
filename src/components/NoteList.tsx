e5e7217f630fe2723451967a2b53328416bf989f
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 21:02:37 -0400

    Add Tags drop down selector to component

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,5 +1,6 @@
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ReactSelect from "react-select/creatable";
 
 // NoteList component
 export function NoteList() {
@@ -28,6 +29,27 @@ export function NoteList() {
             <Form.Group controlId="title"></Form.Group>
             <Form.Label>Title</Form.Label>
             <Form.Control type="text" />
+          </Col>
+          <Col>
+          <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
           </Col>
         </Row>
       </Form>
