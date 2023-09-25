c4994b74bc240ed113fddd64ceb8b6ddff510f3c
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-24 21:31:39 -0400

    Import useOutletContext

--- src/components/NoteLayout.tsx
+++ src/components/NoteLayout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useParams } from "react-router-dom";
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
 import { Note } from "../App"
 
 type NoteLayoutProps = {
