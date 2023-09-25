e0ef7f1b4b5ad568582488a72d6106f2b647f11f
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-24 22:31:03 -0400

    Add Note data as placeholded to EditNote form

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -2,7 +2,6 @@ import { NoteData, Tag } from "../App";
 import { NoteForm } from "./NoteForm";
 import { useNote } from "./NoteLayout";
 
-
 // Define prop types
 type EditNoteProps = {
   onSubmit: (id: string, data: NoteData) => void; // Function to handle note submission
@@ -20,6 +19,9 @@ export function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps) {
       <h1 className="mb-4">Edit Note, Who Dis?</h1>
       {/* Render NoteForm component, passing in props */}
       <NoteForm
+        title={note.title}
+        markdown={note.markdown}
+        tags={note.tags}
         onSubmit={(data) => onSubmit(note.id, data)}
         onAddTag={onAddTag}
         availableTags={availableTags}
