375d65d3c398c667bd40371815e7bdf43a11e16f
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-24 23:07:00 -0400

    Add closeButton and title to modal

--- src/components/EditTagsModal.tsx
+++ src/components/EditTagsModal.tsx
@@ -3,7 +3,9 @@ import { Modal } from "react-bootstrap"
 export function EditTagsModal () {
   return (
     <Modal>
-
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Tags</Modal.Title>
+      </Modal.Header>
     </Modal>
   )
 }
