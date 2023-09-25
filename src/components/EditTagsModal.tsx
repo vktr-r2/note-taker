c43b001ca5577c60cc0c3979f0d2684875673006
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-24 23:10:34 -0400

    Setup modal body layout

--- src/components/EditTagsModal.tsx
+++ src/components/EditTagsModal.tsx
@@ -1,11 +1,28 @@
-import { Modal } from "react-bootstrap"
+import { Button, Col, Form, Modal, Row, Stack } from "react-bootstrap"
 
-export function EditTagsModal () {
+export function EditTagsModal ({ availableTags }) {
   return (
     <Modal>
       <Modal.Header closeButton>
         <Modal.Title>Edit Tags</Modal.Title>
       </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Stack gap={2}>
+            {availableTags.map(tag => {
+              <Row key={tag.id}>
+                <Col>
+                </Col>
+                <Col>
+                  <Button variant="outline-danger">
+                    &times;
+                  </Button>
+                </Col>
+              </Row>
+            })}
+          </Stack>
+        </Form>
+      </Modal.Body>
     </Modal>
   )
 }
