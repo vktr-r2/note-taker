1aa82c07b3b865c139a552f4859048dec68b1083
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-24 21:32:18 -0400

    Return note content in Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,26 @@
-import { useNote } from "./NoteLayout"
+import { Badge, Col, Row, Stack } from "react-bootstrap";
+import { useNote } from "./NoteLayout";
 
 export function Note() {
-  const note = useNote()
+  
+  const note = useNote();
+
+  return (
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
+              {note.tags.map((tag) => (
+                <Badge className="text-truncates" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+      </Row>
+    </>
+  );
 }
